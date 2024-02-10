f1eae2819a3126e8ee0bafbb39317b924845314f
Marat <maratg2develop@gmail.com>
2024-02-10 19:21:00 +0400

    Change expected result

--- bin/main.cpp
+++ bin/main.cpp
@@ -23,7 +23,7 @@ void printArray(const std::vector<float>& arr)
 
 int main() 
 {
-    std::vector<float> arr = { 3/7.0, 1933 / 77.0, 12, 15, 999999, 135/17.0*31, 3, -91/5.0, 1/999999.0, 0, 7/9.0, 3.1415, 35};
+    std::vector<float> arr = { 3/7.0, 1933 / 77.0, 12, 15, 999999, 135/17.0*31, 3, -91/5.0, 1/9999999.0, 0, 7/9.0, 3.1415, 35};
     std::pair<int, int> cs = {3, 6};
     std::vector<float> result_arr = process(arr, cs);
     std::cout << "Source elements: ";
