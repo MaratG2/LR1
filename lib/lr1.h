8413d295073a8644f35611fb6c6e6c6e801ee5a7
Marat <maratg2develop@gmail.com>
2024-02-24 21:08:23 +0400

    Right steps

--- lib/lr1.h
+++ lib/lr1.h
@@ -74,48 +74,62 @@ bool PRINT_STEP(int step)
 
 Result process(const std::vector<float>& arr, const std::pair<float, float>& cs)
 {
-    PRINT_STEP(1);
+    PRINT_STEP(1); //1
     std::vector< std::shared_ptr<Error>> errors_caught; //1
     std::pair<int, float> min(-1, std::nanf("")); //1
     std::vector<float> res = arr; //1
 
-    if (arr.size() > 1024)
+    if (arr.size() > 1024 && PRINT_STEP(2)) //2
     {
-        errors_caught.push_back(ERRORS_MAP[1]);
-        Result results(min, arr, res, errors_caught);
-        return results;
-    }
+        PRINT_STEP(3); //3
+        errors_caught.push_back(ERRORS_MAP[1]); //3
+        Result results(min, arr, res, errors_caught); //3
+        PRINT_STEP(4); //4
+        return results; //4
+    } //4
 
-    bool _notSet = true;
+    PRINT_STEP(5); //5
+    bool _notSet = true; //5
+    int i = 0; //5
 
-    for (int i = 0; i < arr.size(); i++)
+    while (i < arr.size() && PRINT_STEP(6)) //6
     {
-        if (floatLess(arr[i], min.second) || _notSet)
+        if ((floatLess(arr[i], min.second) || _notSet) && PRINT_STEP(7)) //7
         {
-            if (floatMore(arr[i], cs.first))
+            if (floatMore(arr[i], cs.first) && PRINT_STEP(8)) //8
             {
-                if (floatLess(arr[i], cs.second))
+                if (floatLess(arr[i], cs.second) && PRINT_STEP(9)) //9
                 {
-                    min.first = i;
-                    min.second = arr[i];
-                    _notSet = false;
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < arr.size(); i++)
+                    PRINT_STEP(10); //10
+                    min.first = i; //10
+                    min.second = arr[i]; //10
+                    _notSet = false; //10
+                    PRINT_STEP(11); //11
+                } //11
+                PRINT_STEP(12); //12
+            } //12
+            PRINT_STEP(13); //13
+        } //13
+        PRINT_STEP(14); //14
+    } //14
+    PRINT_STEP(15); //15
+    int i = 0; //15
+    while (i < arr.size() && PRINT_STEP(16)) //16
     {
-        if (floatMore(res[i], cs.first))
+        if (floatMore(res[i], cs.first) && PRINT_STEP(17)) //17
         {
-            if (floatLess(res[i], cs.second))
+            if (floatLess(res[i], cs.second) && PRINT_STEP(18)) //18
             {
-                res[i] = min.second;
-            }
-        }
-    }
-
-    Result results(min, arr, res, errors_caught);
-
+                PRINT_STEP(19); //19
+                res[i] = min.second; //19
+                PRINT_STEP(20); //20
+            } //20
+            PRINT_STEP(21); //21
+        } //21
+        PRINT_STEP(22); //22
+    } //22
+    PRINT_STEP(23); //23
+    Result results(min, arr, res, errors_caught); //23
+    PRINT_STEP(4); //4
     return results;
 }
