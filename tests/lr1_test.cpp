762429eabf5b6c4a759f6e2e12b69f5b2577ceb9
Marat <maratg2develop@gmail.com>
2024-02-25 19:41:49 +0400

    Update lr1_test.cpp

--- tests/lr1_test.cpp
+++ tests/lr1_test.cpp
@@ -1,7 +1,12 @@
 #include <lib/lr1.h>
+#include <fstream>
+#include <filesystem>
 #include <gtest/gtest.h>
 #include <vector>
 #include <utility>
+
+const std::string DATA_PATH = "../../LR1/Data/";
+
 
 // Подробнее https://google.github.io/googletest/reference/testing.html
 
@@ -14,8 +19,6 @@ protected:
     void SetUp() override 
     {
         // Например, инициализация данных
-        arr = { 3 / 7.0, 1933 / 77.0, 12, 15, 999999, 135 / 17.0 * 31, 3, -91 / 5.0, 1 / 9999999.0, 0, 7 / 9.0, 3.1415, 35 };
-        cs = { 2, 5 };
     }
 
     // Здесь вы можете освободить ресурсы, если это необходимо
@@ -23,24 +26,39 @@ protected:
     void TearDown() override 
     {
         // Например, очистка данных
-        arr.clear();
     }
 
     // Объявляем переменные, которые будут использоваться в тестах
-    std::vector<float> arr;
-    std::pair<int, int> cs;
-    std::vector<float> result;
 };
 
 // Пример теста1
 TEST_F(ProcessTest, CTest1) 
 {
+    float input_value;
+    std::ifstream data_file(DATA_PATH + "test1.txt");
+    std::vector<float> arr;
+    while (data_file >> input_value)
+        arr.push_back(input_value);
+    std::pair<float, float> cs;
+    cs.second = arr.back();
+    arr.pop_back();
+    cs.first = arr.back();
+    arr.pop_back();
+
 // Вызываем функцию process с тестовыми данными
-    result = process(arr, cs);
+    Result result = process(arr, cs);
 
 // Проверяем ожидаемые результаты
-    std::vector<float> expected_result = { 3 / 7.0, 1933 / 77.0, 12, 12, 12, 12, 3, -91 / 5.0, 1 / 9999999.0, 0, 7 / 9.0, 3.1415, 35 };
-    EXPECT_EQ(result, expected_result);
+    std::vector<float> expected_initial = {};
+    std::vector<float> expected_corrected = {};
+    std::pair<int, float> expected_min(-1, std::nanf(""));
+    std::vector<std::shared_ptr<Error>> errors_caught;
+
+    EXPECT_EQ(result.initial, expected_initial);
+    EXPECT_EQ(result.corrected, expected_corrected);
+    EXPECT_EQ(result.min.first, expected_min.first);
+    EXPECT_EQ(result.min.second, expected_min.second);
+    EXPECT_EQ(result.errors, errors_caught);
 }
 
 // Пример теста2
