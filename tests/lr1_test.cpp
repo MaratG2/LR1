3f1a4d01ddec641cfec5544633d5abdd3aa91529
Marat <maratg2develop@gmail.com>
2024-02-26 20:52:55 +0400

    Add test1

--- tests/lr1_test.cpp
+++ tests/lr1_test.cpp
@@ -31,6 +31,42 @@ protected:
     // Объявляем переменные, которые будут использоваться в тестах
 };
 
+// Тест 1
+TEST_F(ProcessTest, CTest1)
+{
+    std::vector<float> expected_corrected = {};
+    std::vector<float> expected_initial = {};
+
+    float input_value;
+    std::ifstream data_file(DATA_PATH + "test1.txt");
+    std::vector<float> arr;
+    while (data_file >> input_value)
+        arr.push_back(input_value);
+    std::pair<float, float> cs;
+    cs.second = arr.back();
+    arr.pop_back();
+    cs.first = arr.back();
+    arr.pop_back();
+
+    expected_initial = arr;
+    expected_corrected = expected_initial;
+
+    // Вызываем функцию process с тестовыми данными
+    Result result = process(arr, cs);
+
+    // Проверяем ожидаемые результаты
+    std::pair<int, float> expected_min(-1, std::nanf(""));
+    std::vector<std::shared_ptr<Error>> errors_caught = {};
+    errors_caught.push_back(ERRORS_MAP[1]);
+
+    EXPECT_EQ(result.initial, expected_initial);
+    EXPECT_EQ(result.corrected, expected_corrected);
+    EXPECT_EQ(result.min.first, expected_min.first);
+    EXPECT_FALSE(result.min.second == result.min.second);
+    EXPECT_FALSE(expected_min.second == expected_min.second);
+    EXPECT_EQ(result.errors.size(), errors_caught.size());
+}
+
 // Тест 2
 TEST_F(ProcessTest, CTest2) 
 {
