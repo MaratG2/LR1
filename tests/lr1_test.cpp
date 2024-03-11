cf09259788d1d82d64b61562602d5ded97167952
Marat <maratg2develop@gmail.com>
2024-03-11 20:51:42 +0400

    Update lr1_test.cpp

--- tests/lr1_test.cpp
+++ tests/lr1_test.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <utility>
 
-const std::string DATA_PATH2 = "home/runner/work/LR1/LR1/build/data/";
+const std::string DATA_PATH2 = "home/runner/work/LR1/LR1/build/tests/data/";
 
 
 // Подробнее https://google.github.io/googletest/reference/testing.html
